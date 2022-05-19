8767fcf5cbc72abf3ef66d5e1e138bda586713b5
NPimentel02 <105312666+NPimentel02@users.noreply.github.com>
2022-05-19 11:25:51 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,61 @@
+function countdownTimerV1(){
+    console.log("countdownTimerV1() begin");
+    var counter = 20;
+    document.getElementById("countdownDisplay").innerHTML = counter;
+    console.log<(counter);
+    setTimeout(function(){
+        counter = counter - 5;
+        document.getElementById("countdownDisplay").innerHTML = counter;
+        console.log<(counter);
+    }, 2000)
+    setTimeout(function(){
+        counter = counter - 5;
+        document.getElementById("countdownDisplay").innerHTML = counter;
+        console.log<(counter);
+    }, 4000)
+    setTimeout(function(){
+        counter = counter - 5;
+        document.getElementById("countdownDisplay").innerHTML = counter;
+        console.log<(counter);
+    }, 5000)
+    setTimeout(function(){
+        counter = counter - 5;
+        document.getElementById("countdownDisplay").innerHTML = counter;
+        console.log<(counter);
+    }, 6000)
+    setTimeout(function(){
+        counter = counter - 5;
+        document.getElementById("countdownDisplay").innerHTML = counter;
+        console.log<(counter);
+    }, 8000)
+    setTimeout(function(){
+        counter = counter - 5;
+        document.getElementById("countdownDisplay").innerHTML = counter;
+        console.log<(counter);
+    }, 10000)
+    setTimeout(function(){
+        counter = counter - 5;
+        document.getElementById("countdownDisplay").innerHTML = counter;
+        console.log<(counter);
+    }, 12000)
+    setTimeout(function(){
+        counter = counter - 5;
+        document.getElementById("countdownDisplay").innerHTML = counter;
+        console.log<(counter);
+    }, 14000)
+    setTimeout(function(){
+        counter = counter - 5;
+        document.getElementById("countdownDisplay").innerHTML = counter;
+        console.log<(counter);
+    }, 16000)
+    setTimeout(function(){
+        counter = counter - 5;
+        document.getElementById("countdownDisplay").innerHTML = counter;
+        console.log<(counter);
+    }, 18000)
+    setTimeout(function(){
+        counter = counter - 5;
+        document.getElementById("countdownDisplay").innerHTML = Blastoff;
+        console.log<(counter);
+    }, 20000)
+}
